4c32807164e6cbf36f0b5e9133daba24d7f4d213
Chris Callan <chriscallan@comcast.net>
2016-07-12 08:57:02 -0700

    Final implementation of the solution for problem statement #39 of Project Euler;

--- 039_IntegerRightTriangles.py
+++ 039_IntegerRightTriangles.py
@@ -1,10 +1,33 @@
 # Problem 39
 # If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three
-#       rsolutions for p = 120.
+#       resolutions for p = 120.
 #
 # {20,48,52}, {24,45,51}, {30,40,50}
 #
 # For which value of p ≤ 1000, is the number of solutions maximised?
 
 
+
+def find_pyth_set(in_perimeter):
+    retval = []
+    # found the mathematical proof here
+    #   codereview.stackexchange.com/questions/49413/project-euler-39-integer-right-triangles
+    for a in range(3, (in_perimeter // 2) - 1):
+        c = ((a ** 2) + ((in_perimeter - a) ** 2)) // (2 * (in_perimeter - a))
+        b = ((c ** 2) - (a ** 2)) ** 0.5
+        # make sure the square root is an integer/whole number
+        if b.is_integer() and (a + b + c) == in_perimeter:
+            retval.append((a, int(b), c, ))
+    return retval
+# this holds the potential candidate values to answer the question
+final_answer = {'perimeter': 0, 'count': 0, 'set': []}
+for perimeter in range(1000):
+    working_set = find_pyth_set(perimeter)
+    if len(working_set) > final_answer['count']:
+        final_answer['perimeter'] = perimeter
+        final_answer['count'] = len(working_set)
+        final_answer['set'] = working_set
+
+print("final_answer: {0} count is: {1} and the set is: {2}".format(str(final_answer['perimeter']),
+                                                                   str(final_answer['count']), final_answer['set']))
 
