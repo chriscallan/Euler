1a219d738b8649eb77b7f6f683001dd6f620f992
Chris Callan <chriscallan@comcast.net>
2016-07-22 10:13:46 -0700

    Added method comments to the is_pandigital() method, so that it's more obvious how the method is used; Inserted a new method to generate prime number lists, derived from an online method;      - changed some of the syntax to be more intuitively obvious what it's doing;

--- helpers/helper_methods.py
+++ helpers/helper_methods.py
@@ -1,4 +1,5 @@
 import re
+from math import sqrt, ceil
 
 
 class helper_methods(object):
@@ -23,7 +24,37 @@ class helper_methods(object):
         return retval
 
     @staticmethod
+    def generate_prime_list(in_number):
+        """
+        Helper method to generate a list of prime numbers
+            Pulled from: http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n/2073279
+            This implementation was one of the faster implementations, with or without JIT-compilation
+            I've modified it to include more explicit execution hints, by way of parentheses, exponents,
+                native functions, etc...
+        :param in_number: upper limit for the list of primes (aka. all_primes[] <= in_number)
+        :return: a list object populated with prime numbers from 2 through 'in_number'
+        """
+        sieve = [True] * in_number
+        for i in range(3, ceil(sqrt(in_number)), 2):
+            if sieve[i]:
+                # work through the sieve with a stride of 2*i
+                sieve[i ** 2::2 * i] = [False] * (((in_number - (i ** 2) - 1) // (2 * i)) + 1)
+        return [2] + [i for i in range(3, in_number, 2) if sieve[i]]
+
+    @staticmethod
     def is_pandigital(in_number, lowest_digit=1, length=9):
+        """
+        Helper method to determine if the provided value is a "pandigital" number
+            E.g. - "987654321" is 1-9 pangital, and 876543213 is not
+        :param in_number: the number you want to check for "pandigitalness"
+        :param lowest_digit: the lowest digit that should be included in your pangigital set
+            usually '1' is the lowest values to search for, but other digits can be used ('0' is the next most common)
+        :param length: the length of the pandigital set
+            usually '9' is the length of the set, but lower values could be used also
+        :return: bool indicating if the provided number fit the pandigital rules and any additional constraints
+        """
+        if 0 < length >= 9:
+            raise Exception("length parameter: {0} was incorrect.".format(length))
         re_checker = re.compile("^(?!.*([{0}-{1}]).*\\1)[{0}-{1}]{{{0}}}$".format(lowest_digit, length))
         return True if len(re_checker.findall(str(in_number))) is not 0 else False
 
